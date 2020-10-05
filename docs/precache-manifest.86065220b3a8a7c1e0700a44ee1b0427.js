a2e2168643a34338354b00c3251adeacb827038b
WookieR <matiasroldanrma@gmail.com>
2020-10-05 19:02:25 -0300

    Docs agregado

---
+++ docs/precache-manifest.86065220b3a8a7c1e0700a44ee1b0427.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6bb735d60e20e2135087d0da5188b90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3861c208c92e6f19307b",
+    "url": "/static/css/main.38c0a145.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3861c208c92e6f19307b",
+    "url": "/static/js/main.e3d62f8d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
